b7e6a75ecb3b9715a057b8081a89e856e00813ae
Sachin <sachinbhatt7115@gmail.com>
2026-06-15 21:26:41 +0530

    Added Google verification and fixed images path

--- js.js
+++
@@ -1,9 +0,0 @@
-script.js
-const input = document.getElementById('input');
-const generate = document.getElementById('generate');
-const output = document.getElementById('output');
-
-generate.addEventListener('click', () => {
-  console.log('Generate button clicked!');
-  output.textContent = input.value;
-});
